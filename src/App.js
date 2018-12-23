de1bc93ad1ea2b8e2eebe6704cb79beb0a9e1c71
ashishKv <ashishverma293@gmail.com>
2018-12-24 00:03:08 +0530

    Changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Just a change!! Test
+            I was changed on feature branch.
           </p>
           <a
             className="App-link"
